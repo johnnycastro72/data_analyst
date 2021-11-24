5a3eff7d0ad4110c19cec6612f025dee687a1a18
Jhonny Castro Clavijo <johnny.castro@misena.edu.co>
2021-11-24 00:02:20 -0500

    Creating a project

--- example2.py
+++ example2.py
@@ -1,6 +1,9 @@
 from bs4 import BeautifulSoup
 import requests
 
+print('Print a skill that you are not familiar with')
+unfamiliar_skill = input('>')
+print(f'Filtering out {unfamiliar_skill}')
 html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
 soup = BeautifulSoup(html_text, 'lxml')
 jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
@@ -10,8 +13,10 @@ for job in jobs:
     if 'few' in published_date or 'today' in published_date:
         company_name = job.find('h3', class_='joblist-comp-name').text.strip()
         skills = job.find('span', class_='srp-skills').text.strip()
+        more_info = job.header.h2.a['href']
 
-        print(f'''
-        Company Name: {company_name}
-        Required Skills: {skills}
-        ''')
+        if unfamiliar_skill not in skills:
+            print(f"Company Name: {company_name}")
+            print(f"Required Skills: {skills}")
+            print(f"More info: {more_info}")
+            print('')
