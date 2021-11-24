5d72a43c33ce28f01a9f9181983db21076213718
Jhonny Castro Clavijo <johnny.castro@misena.edu.co>
2021-11-23 22:52:08 -0500

    First example finished

--- example.py
+++ example.py
@@ -4,7 +4,9 @@ with open('home.html', 'r') as html_file:
     content = html_file.read()
 
     soup = BeautifulSoup(content, 'lxml')
-    courses_html_tags = soup.findAll('h5')
-    for course in courses_html_tags:
-        print(course.text)
-        
+    courses_cards = soup.findAll('div', class_='card')
+    for course in courses_cards:
+        course_name = course.h5.text
+        course_price = course.a.text.split()[-1]
+
+        print(f"{course_name} costs {course_price}")
